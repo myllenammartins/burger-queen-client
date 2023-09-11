d4685ef9e016841ad6ee091846819692eca5dd15
unknown <mirandamartins05@gmail.com>
2023-09-11 10:36:26 -0300

    CHORE: trabalhando na tela de login

--- burgerQueen/src/app/components/login/login.component.ts
+++ burgerQueen/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  loginError = false;
 
+  constructor(private router: Router) {}
+
+  onSubmit(email: string, password: string) {
+    // Simule a autenticação (substitua com sua lógica real)
+    if (email === 'seu_email_correto' && password === 'sua_senha_correta') {
+      // Credenciais corretas, redirecione para a rota de pedidos
+      this.router.navigate(['/pedidos']);
+    } else {
+      // Credenciais incorretas, mostre o erro
+      this.loginError = true;
+    }
+  }
 }
