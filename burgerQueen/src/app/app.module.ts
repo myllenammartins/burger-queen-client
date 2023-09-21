c2ec6d05e28b16a719618edec0f3ace365d497cf
unknown <mirandamartins05@gmail.com>
2023-09-21 15:41:28 -0300

    FEAT: config da pasta auth.service e login.component

--- burgerQueen/src/app/app.module.ts
+++ burgerQueen/src/app/app.module.ts
@@ -2,22 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/commons/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { KitchenComponent } from './components/kitchen/kitchen.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    LoginComponent,
+    KitchenComponent,
+    MenuComponent,
+    AdminComponent,
   
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+    CommonModule,
     FormsModule,
     BrowserAnimationsModule
   ],
--- burgerQueen/src/app/service/auth/auth.service.ts
+++ burgerQueen/src/app/service/auth/auth.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { firstValueFrom } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -7,5 +11,20 @@ export class AuthService {
 
   public apiUrl = 'https://burger-api-fawn.vercel.app/'
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) { }
+
+  async login (email: string, password: string): Promise<boolean> {
+
+    const userLogin = { email, password }
+    const response = await firstValueFrom(this.http.post<any>(this.apiUrl, userLogin));
+
+    if (response.token) {
+      localStorage.setItem('token', response.token); 
+      localStorage.setItem('userEmail', email);
+      localStorage.setItem('userRole', response.role);
+      return true;
+    } else {
+      throw new Error('Login inválido');
+    }
+  }
 }
